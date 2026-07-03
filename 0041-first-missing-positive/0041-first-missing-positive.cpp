249c41610fecc93f04edaf4a0eb3047761ca6b4a
Advikk <saksenaadvik@gmail.com>
2026-07-03 23:32:13 +0530

    Time: 0 ms (100%), Space: 55 MB (85.51%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -11,12 +11,16 @@ public:
                 break;
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1)
-                return i + 1;
+    int i=0;
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1){
+                break;
+            }
         }
 
-        return n + 1;
+       
+     return i+1;
+
+
     }
 };

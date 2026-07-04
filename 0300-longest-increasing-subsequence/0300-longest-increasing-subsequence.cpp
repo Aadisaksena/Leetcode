5e878b0dcab46cb79082eabb51b1ffb193006a3e
Advikk <saksenaadvik@gmail.com>
2026-07-04 10:44:01 +0530

    Time: 73 ms (49.82%), Space: 14.4 MB (47.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
-        return solve(nums,dp,0,-1);
-    }
-    int solve(vector<int>& nums,vector<vector<int>>& dp, int index,int last_index){
-        
-        if(index==nums.size())
-        return 0;
+        int n = nums.size();
 
-        if(dp[index][last_index+1]!=-1)
-        return dp[index][last_index+1];
+        vector<int> dp(n, 1);   // Every element itself is an LIS of length 1
+        int ans = 1;
 
-        //ek element pr hain ab agar eo humare pichle element se bada hai then we take it or skip it
-        if(last_index==-1 || nums[index]>nums[last_index]){
-           int take=1+ solve(nums,dp,index+1,index);
-           int skip=solve(nums,dp,index+1,last_index);
-           dp[index][last_index+1]=max(take,skip);
-        }
-        else{//we skip it
-            dp[index][last_index+1]=solve(nums,dp,index+1,last_index);
+        for (int i = 1; i < n; i++) {
+
+            // Try extending the LIS ending at every previous index
+            for (int j = 0; j < i; j++) {
+
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+
+            }
+
+            ans = max(ans, dp[i]);
         }
 
-        return dp[index][last_index+1];
+        return ans;
     }
 };

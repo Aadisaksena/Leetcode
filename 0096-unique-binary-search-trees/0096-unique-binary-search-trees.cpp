f5b1c6af005f95ddd7ac4ad31758040763183d20
Advikk <saksenaadvik@gmail.com>
2026-08-18 01:27:48 +0530

    Time: 2 ms (15.05%), Space: 8.2 MB (19.77%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int numTrees(int n) {
-        return uniqtre(n);
+        vector<int> dp(n+1,-1);
+        return uniqtre(n,dp);
     }
-    int uniqtre(int n){
+    int uniqtre(int n,vector<int>& dp){
         if(n==0)return 1;
         if(n==1)return 1;
+        if(dp[n]!=-1)return dp[n];
         int ans=0;
         for(int i=1;i<=n;i++){
             //for a given index
-            ans+=uniqtre(i-1)*uniqtre(n-i);
+            ans+=uniqtre(i-1,dp)*uniqtre(n-i,dp);
         }
-
-        return ans;
+        dp[n]=ans;
+        return dp[n];
     }
 };
